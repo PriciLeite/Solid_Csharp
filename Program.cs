fbb6acb22ef0525aa521aaf393cb63726501c571
Priscila Leite <pricila_leite@hotmail.com>
2024-07-28 19:08:10 -0300

    Aplicando sobrecarga de Métodos

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Solid
 {
     class program
     {
+        static public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static public double Somar(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        // Recebendo um Array de parâmetros.
+        static public int Somar(params int[] numeros)
+        {
+            var resultado = 0;
+            foreach (var num in numeros)
+                resultado += num;
+            return resultado;
+        }
+
+
         static void Main(string[] args)
         {
-            //Generalist builder:
-            People p1 = new People("John", "masculino", 18);
+            var resultado1 = Somar(100, 200);
+            var resultado2 = Somar(100, 200, 100);
+            var resultado3 = Somar(new int[] { 1, 2, 3 });
+            //Simplificada.
+            var resultado4 = Somar(1, 2, 3);
 
-            System.Console.WriteLine(p1.Name);
-            System.Console.WriteLine(p1.Gender);
-
+            System.Console.WriteLine(resultado1);
+            System.Console.WriteLine(resultado2);
+            System.Console.WriteLine(resultado3);
+            System.Console.WriteLine(resultado4);
 
 
 
