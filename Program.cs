1118fe511fc9ac83a4399ad8548ad25bca31ac06
Priscila Leite <pricila_leite@hotmail.com>
2024-07-29 17:00:11 -0300

    Como usar Parâmetros Opcionais nos Métodos

--- Program.cs
+++ Program.cs
@@ -5,41 +5,23 @@ namespace Solid
 {
     class program
     {
-        static public int Somar(int num1, int num2)
+        // Aplicando: Métodos com parâmetros opcioanais.
+        static public int Somar(int num1, int num2 = 20, int num3 = 30, int num4 = 40)
         {
-            return num1 + num2;
+            return num1 + num2 + num3 + num4;
         }
-
-        static public double Somar(int num1, int num2, int num3)
-        {
-            return num1 + num2 + num3;
-        }
-
-        // Recebendo um Array de parâmetros.
-        static public int Somar(params int[] numeros)
-        {
-            var resultado = 0;
-            foreach (var num in numeros)
-                resultado += num;
-            return resultado;
-        }
-
 
         static void Main(string[] args)
         {
-            var resultado1 = Somar(100, 200);
-            var resultado2 = Somar(100, 200, 100);
-            var resultado3 = Somar(new int[] { 1, 2, 3 });
-            //Simplificada.
-            var resultado4 = Somar(1, 2, 3);
+            var resultado1 = Somar(100);
+            var resultado2 = Somar(100, 100);
+            var resultado3 = Somar(100, 100, 100);
+            var resultado4 = Somar(100, 100, 100, 100);
 
             System.Console.WriteLine(resultado1);
             System.Console.WriteLine(resultado2);
             System.Console.WriteLine(resultado3);
             System.Console.WriteLine(resultado4);
-
-
-
         }
     }
 }
