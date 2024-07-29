bd2834abd8468e51bac28e46e07aab190bc9b704
Priscila Leite <pricila_leite@hotmail.com>
2024-07-29 19:16:26 -0300

    Métodos Statics: Implementando conceito de static.

---
+++ Entities/Calculadora.cs
@@ -0,0 +1,14 @@
+public class Calculadora
+{
+    public static int Somar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int Subtracao(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -5,15 +5,11 @@ namespace Solid
 {
     class program
     {
-        // Aplicando: Métodos com Argumentos Nomeados.
-        static void Enviar(string destino, string titulo, string assunto)
-        {
-            System.Console.WriteLine($"\n{destino} - {titulo} - {assunto}\n");
-        }
-
         static void Main(string[] args)
         {
-            Enviar(titulo: "Contrato", assunto: "Responder ao receber este e-mail.", destino: "prici_leite@gmail.com");
+            // Chamando o método sem instânciar um objeto:            
+            System.Console.WriteLine($"Somar: {Calculadora.Somar(2, 5)}");
+            System.Console.WriteLine($"Subtrair: {Calculadora.Subtracao(5, 2)}");
         }
     }
 }
