29449a12e511a35d3997f0174da6ca8b03a0cd24
Priscila Leite <pricila_leite@hotmail.com>
2024-09-05 19:52:47 -0300

    Aplicando coneitos básicos de Herança. ContaPoupanca:Conta

--- Program.cs
+++ Program.cs
@@ -1,23 +1,58 @@
 ﻿using System;
+using System.Globalization;
+using System.Security.Claims;
 using Solid.Utils;
 
 
-namespace Solid
+namespace Solid;
+
+class program
 {
-    class program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var texto1 = "curso";
-            var texto2 = "maria";
-
-            // método de extensão
-            texto1 = texto1.CaixaAltaPrimeiraLetra();
-            texto2 = texto2.CaixaAltaPrimeiraLetra();
-
-            System.Console.WriteLine(texto1);
-            System.Console.WriteLine(texto2);
-        }
+        ContaPoupanca cp = new ContaPoupanca();
+        cp.Numero = 001;
+        cp.Saldo = 1000.00;
+        cp.GetSaldo();
+        cp.Saque();
     }
+
+    class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+        public void GetSaldo()
+        {
+            System.Console.WriteLine($"Seu saldo atual R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        public void Saque()
+        {
+            System.Console.Write("Digite valor para saque: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            if (Saldo > 0 && Saldo > valor)
+            {
+                Saldo = Saldo - valor;
+                System.Console.WriteLine($"Seu saldo atual é R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}\n");
+            }
+            else
+            {
+                throw new Exception($"Saldo {Saldo} insuficiente! ");
+            }
+
+        }
+
+    }
+
+    class ContaPoupanca : Conta
+    {
+        public int JurosMensais { get; set; }
+    }
+
+
 }
+
+
+
 
