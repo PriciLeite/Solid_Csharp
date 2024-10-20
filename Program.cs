74e014f968924a2537638fc24920a0f1d0a0ada7
Priscila Leite <pricila_leite@hotmail.com>
2024-10-20 16:57:37 -0300

    Como herdar o construtor de uma propriedade 'private' da classe base pela classe derivada ?

--- Program.cs
+++ Program.cs
@@ -5,66 +5,35 @@ namespace Solid;
 
 class program
 {
-
-    class Conta
+    public class Veiculo
     {
-        public Conta()
+        private string numeroChasis { get; set; }
+
+        public Veiculo(string numeroChassis)
         {
-            System.Console.WriteLine("Testando se a classe derivada irá excutar primeiro o construtor da classe base");
+            this.numeroChasis = numeroChassis;
+            System.Console.WriteLine($"Inicializando Veiculo: {numeroChasis}");
         }
+    }
 
-        public int Numero { get; set; }
-        public double Saldo { get; set; }
-
-
-
-        protected void GetSaldo()
+    public class Carro : Veiculo
+    {
+        public Carro(string numeroChassis) : base(numeroChassis)
         {
-            System.Console.WriteLine($"Seu saldo atual R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
-        }
-
-
-
-        internal void Saque()
-        {
-            System.Console.Write("Digite valor para saque: ");
-            double valor = double.Parse(Console.ReadLine());
-
-            if (Saldo > 0 && Saldo > valor)
-            {
-                Saldo = Saldo - valor;
-                System.Console.WriteLine($"Seu saldo atual é R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}\n");
-            }
-            else
-            {
-                throw new Exception($"\nSeu saldo de R${Saldo.ToString("F2", CultureInfo.InvariantCulture)} é insuficiente!\n");
-            }
-
+            System.Console.WriteLine($"Inicializando Carro: {numeroChassis}");
         }
 
     }
 
-    class ContaPoupanca : Conta
+
+    static void Main(string[] args)
     {
-        public int JurosMensais { get; set; }
-
-        protected new void GetSaldo()
-        {
-            System.Console.WriteLine("Método (Saldo) da classe base foi ocultado por uso da palavra 'new'");
-        }
-
-        static void Main(string[] args)
-        {
-            ContaPoupanca cp = new ContaPoupanca();
-            cp.Numero = 001;
-            cp.Saldo = 1000.00;
-            cp.GetSaldo();
-            cp.Saque();
-        }
+        var carro = new Carro("E34910");
     }
-
-
 }
+
+
+
 
 
 
